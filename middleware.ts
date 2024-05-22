b3850388808e0a2ce414065e3801a7d491e6cc7a
Sumit <sumitsapkota0@gmail.com>
2024-05-22 07:17:25 -0500

    fix: added middleware for authorized route handling

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+const protectedRoutes = ["/home"];
+const loginPath = "/auth/login";
+
+export default async function AuthMiddleware(req: NextRequest) {
+  const url = req.nextUrl;
+  // Check if the requested route is protected
+  const isProtectedRoute = protectedRoutes.some((route) =>
+    url.pathname.startsWith(route)
+  );
+  const myCookie = cookies();
+  const token = myCookie.get("next-auth.session-token");
+  const isLoggedIn = token && token.value;
+  // Handle protected routes
+  if (isProtectedRoute && !isLoggedIn) {
+    return NextResponse.redirect(new URL(loginPath, req.url));
+  }
+
+  // Allow access to non-protected routes or homepage if logged in
+  return NextResponse.next();
+}
